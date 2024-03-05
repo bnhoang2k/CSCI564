9fb1a45d682953a1d066f1e6ae03c8790420d26f
bryanhoang <bryanhoang@mines.edu>
2024-03-05 01:17:03 -0700

    trying to implement lru

--- Projects/project1-master/src/replacement_policies.c
+++ Projects/project1-master/src/replacement_policies.c
@@ -18,25 +18,82 @@
 // LRU Replacement Policy
 // ============================================================================
 // TODO feel free to create additional structs/enums as necessary
+// Create a linked list for each set to keep track of the order of the cache lines
+
+struct lru_node {
+    uint32_t tag;
+    struct lru_node *next;
+    struct lru_node *prev;
+};
 
 uint32_t lru_eviction_index(struct replacement_policy *replacement_policy,
                             struct cache_system *cache_system, uint32_t set_idx)
 {
     // TODO return the index within the set that should be evicted.
-    return 0;
+    // We want to remove the last element in the linked list
+    struct lru_node *set_head = ((struct lru_node **)replacement_policy->data)[set_idx];
+    uint32_t eviction_index = 0;
+    while (set_head->next != NULL) {
+        set_head = set_head->next;
+        eviction_index++;
+    }
+    return eviction_index;
 }
 
 void lru_cache_access(struct replacement_policy *replacement_policy,
                       struct cache_system *cache_system, uint32_t set_idx, uint32_t tag)
 {
     // TODO update the LRU replacement policy state given a new memory access
-    
+    struct lru_node *set_head = ((struct lru_node **)replacement_policy->data)[set_idx];
+
+    // First, if the set is empty, we'll just add the tag to the set
+    if ((set_head->next == NULL) && (set_head->prev == NULL)) {
+        set_head->tag = tag;
+    }
+    else {
+        // Two scenarios occur: the tag is already in the set or it's not
+        // Our plan is to move recently used tags to the front of the linked list and delete the last tag
+        struct lru_node *temp = set_head;
+        bool tag_found = false;
+        while (temp->next != NULL) {
+            if (temp->tag == tag) {
+                tag_found = true;
+                break;
+            }
+            temp = temp->next;
+        }
+        if (tag_found) {
+            struct lru_node *current = set_head;
+            while (current->next != NULL) {
+                if ((current->tag == tag) && (current != set_head)) {
+                    // Move the tag to the front of the linked list
+                    current->prev = current->next;
+                    current->next = current->prev;
+                    current->next = set_head;
+                    current->prev = NULL;
+                    set_head->prev = current;
+                    set_head = current;
+                }
+                current = current->next;
+            }
+        }
+        else {
+            // If the tag is not in the set, we'll add it to the front of the linked list
+            struct lru_node *new_node = malloc(sizeof(struct lru_node));
+            new_node->tag = tag;
+            new_node->next = set_head;
+            new_node->prev = NULL;
+            set_head->prev = new_node;
+            set_head = new_node;
+        }
+
+    }
 }
 
 void lru_replacement_policy_cleanup(struct replacement_policy *replacement_policy)
 {
     // TODO cleanup any additional memory that you allocated in the
-    // lru_replacement_policy_new function.
+    // lru_replacement_policy_new function.sudo
     free(replacement_policy->data);
 }
 
@@ -50,6 +107,17 @@ struct replacement_policy *lru_replacement_policy_new(uint32_t sets, uint32_t as
     // TODO allocate any additional memory to store metadata here and assign to
     // lru_rp->data.
 
+    // Create an array of structs to store the linked list for each set
+    // However, the array has to be a double pointer to be able to store the head of the linked lists.
+    struct lru_node **lru_lists = calloc(sets, sizeof(struct lru_node*));
+    for (int i = 0; i < sets; i++) {
+        struct lru_node *head = malloc(sizeof(struct lru_node));
+        head->tag = 0;
+        head->next = NULL;
+        head->prev = NULL;
+        lru_lists[i] = head;
+    }
+    lru_rp->data = lru_lists;
     return lru_rp;
 }
 
